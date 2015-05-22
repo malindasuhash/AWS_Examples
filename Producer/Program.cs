b0223a9cf40c1485aacc9665e40037e859860c1a
malindasuhash <malinda.suhash@gmail.com>
2015-05-22 22:04:37 +0100

    sending the consumer name from commandline

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -11,13 +11,13 @@ namespace Producer
     {
         static void Main(string[] args)
         {
-            SendMessages();
+            SendMessagesPlain(args[0]);
 
             Console.WriteLine("Waiting to complete....");
             Console.ReadKey();
         }
 
-        private static async Task SendMessages()
+        private static async Task SendMessagesPlain(string consumerName)
         {
             Amazon.SQS.AmazonSQSClient client = new Amazon.SQS.AmazonSQSClient();
 
@@ -29,14 +29,14 @@ namespace Producer
                 var message = new Amazon.SQS.Model.SendMessageRequest()
                 {
                     QueueUrl = "https://sqs.eu-west-1.amazonaws.com/903119102693/MyQueue",
-                    MessageBody = string.Format("Number-{0}-{1}", DateTime.Now.Millisecond, random.Next())
+                    MessageBody = string.Format("({2}) Number-[{0}-{1}]", DateTime.Now.Millisecond, random.Next(), consumerName)
                 };
 
                 await client.SendMessageAsync(message, cancellationTokenSource.Token);
 
                 Console.WriteLine("Sending message - {0}", message.MessageBody);
 
-                if (i == 50)
+                if (i == 50) // Attempt to simulate a cancellation halfway.
                 {
                     cancellationTokenSource.Cancel();
 
