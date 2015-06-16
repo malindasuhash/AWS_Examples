84135b750f539c3f938b3ac38615753a887dbdd8
malinda.karunaratne <malinda.karunaratne@easyjet.com>
2015-06-16 09:30:05 +0100

    Adding missed files

---
+++ Examples.Core/ConsoleWaiter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Examples.Core
+{
+    public class ConsoleWaiter
+    {
+        public void WaitFor(ConsoleKey key, Action onExit, int waitForMiliseconds = 5000)
+        {
+            Console.WriteLine("\rPress {0} to stop!", key);
+
+            var keyInfo = Console.ReadKey();
+
+            while (keyInfo.Key != key)
+            {
+                Thread.Sleep(waitForMiliseconds);
+
+                if (keyInfo.Key == key)
+                {
+                    onExit();
+                    break;
+                }
+
+                keyInfo = Console.ReadKey();
+            }
+        }
+
+        public void Message(string message)
+        {
+            Console.Write("\r{0}", message);
+        }
+    }
+}
