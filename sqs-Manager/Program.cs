9903e19de4ebb64af550a84e958fcb697e7c05fe
malinda.karunaratne <malinda.karunaratne@easyjet.com>
2015-06-19 15:33:51 +0100

    Adding a simple manager code to monitor the queue details.

---
+++ sqs-Manager/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Examples.Core;
+
+namespace sqs_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var waiter = new ConsoleWaiter();
+
+            var showInfo = new ShowSqsInfo();
+            showInfo.Show();
+
+            waiter.WaitFor(ConsoleKey.V, () => {});
+        }
+    }
+}
---
+++ sqs-Manager/ShowSqsInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace sqs_Manager
+{
+    public class ShowSqsInfo
+    {
+        public void Show()
+        {
+            var client = new AmazonSQSClient();
+            client.Config.RegionEndpoint = RegionEndpoint.EUWest1;
+            
+            // queue details
+            var queueName = ConfigurationManager.AppSettings["queueName"];
+
+            // queue url req
+            var queueUrl = new GetQueueUrlRequest(queueName);
+            var queueUrlResult = client.GetQueueUrl(queueUrl);
+            
+            Console.WriteLine("Url {0}", queueUrlResult.QueueUrl);
+        }
+    }
+}
