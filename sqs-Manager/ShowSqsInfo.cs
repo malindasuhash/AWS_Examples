ed9baadabbd88a11f05fff6ac35c0e23a057ea67
malinda.karunaratne <malinda.karunaratne@easyjet.com>
2015-06-19 15:57:40 +0100

    Addded few more bits.

--- sqs-Manager/ShowSqsInfo.cs
+++ sqs-Manager/ShowSqsInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using Amazon;
 using Amazon.SQS;
@@ -8,19 +9,26 @@ namespace sqs_Manager
 {
     public class ShowSqsInfo
     {
-        public void Show()
+        public async void Show()
         {
             var client = new AmazonSQSClient();
             client.Config.RegionEndpoint = RegionEndpoint.EUWest1;
             
             // queue details
-            var queueName = ConfigurationManager.AppSettings["queueName"];
+            var simpleQueueName = ConfigurationManager.AppSettings["queueName"];
 
             // queue url req
-            var queueUrl = new GetQueueUrlRequest(queueName);
+            var queueUrl = new GetQueueUrlRequest(simpleQueueName);
             var queueUrlResult = client.GetQueueUrl(queueUrl);
             
-            Console.WriteLine("Url {0}", queueUrlResult.QueueUrl);
+            Console.WriteLine("Url: {0}", queueUrlResult.QueueUrl);
+
+            // Details about queues
+            var queues = await client.ListQueuesAsync(new ListQueuesRequest());
+            foreach (var urls in queues.QueueUrls)
+            {
+                Console.WriteLine("Queue url: {0}", urls);
+            }
         }
     }
 }
